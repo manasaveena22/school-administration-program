2e5396c81288ec8bf5594f24c919f95010578f18
manasaveena22 <92075064+manasaveena22@users.noreply.github.com>
2021-12-30 20:42:14 +0530

    Add files via upload
    school administration program is to enable the user to enter the student data, pre-process the given data.

---
+++ admistration.py
@@ -0,0 +1,58 @@
+import csv
+
+student_fields = [ 'name', 'age', 'email', 'phone']
+student_database = 'students.csv'
+
+
+def display_menu():
+    print("1. Add New Student")
+    print("2. View Students")
+
+     
+def add_student():
+    print("Add Student Information")
+    global student_fields
+    global student_database
+
+    student_data = []
+    for field in student_fields:
+        value = input("Enter " + field + ": ")
+        student_data.append(value)
+
+    with open(student_database, "a", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerows([student_data])
+
+    print("Data saved successfully")
+    input("Press any key to continue")
+    return
+
+     
+def view_students():
+    global student_fields
+    global student_database
+
+    print("--- Student Records ---")
+
+    with open(student_database, "r", encoding="utf-8") as f:
+        reader = csv.reader(f)
+        for x in student_fields:
+            print(x, end='\t |')
+        print("\n-----------------------------------------------------------------")
+
+        for row in reader:
+            for item in row:
+                print(item, end="\t |")
+            print("\n")
+
+    input("Press any key to continue")
+
+while True:
+    display_menu()
+
+    choice = input("Enter your choice: ")
+    if choice == '1':
+        add_student()
+    elif choice == '2':
+        view_students()
+        break
